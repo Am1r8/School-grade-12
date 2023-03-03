4afcb7b62362a49c64a8964f49f63e31ce16e19d
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-03 12:25:42 -0500

    new assignment

---
+++ grade 12/rainbow/Rainbow.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Rainbow extends JPanel
+{
+    private final Color skyColor = Color.CYAN;
+
+
+
+}
