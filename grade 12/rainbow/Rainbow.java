2d6a047901f131875ff30d26365268f63f4d6822
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-03 12:25:57 -0500

    new assignment

--- grade 12/rainbow/Rainbow.java
+++ grade 12/rainbow/Rainbow.java
@@ -8,6 +8,21 @@ public class Rainbow extends JPanel
 {
     private final Color skyColor = Color.CYAN;
 
+    public Rainbow()
+    {
+        setBackground(skyColor);
+    }
+
+    // Draws the rainbow.
 
 
+    public static void main(String[] args)
+    {
+        JFrame w = new JFrame("Rainbow");
+        w.setBounds(300, 300, 300, 200);
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = w.getContentPane();
+        c.add(new Rainbow());
+        w.setVisible(true);
+    }
 }
