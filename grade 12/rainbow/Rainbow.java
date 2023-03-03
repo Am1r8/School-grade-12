38b3021033d7262501ff117188fe44fc7db11d25
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-03 12:26:52 -0500

    new

--- grade 12/rainbow/Rainbow.java
+++ grade 12/rainbow/Rainbow.java
@@ -14,7 +14,46 @@ public class Rainbow extends JPanel
     }
 
     // Draws the rainbow.
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        int width = getWidth();
+        int height = getHeight();
 
+        // Declare and initialize local int variables xCenter, yCenter
+        // that represent the center of the rainbow rings:
+        int xCenter = width/2;
+        int yCenter = (height * 3) /4;
+        // Declare and initialize the radius of the large semicircle:
+        int largeRadius = width / 4;
+
+        g.setColor(Color.RED);
+
+        // Draw the large semicircle:
+        g.fillArc( xCenter - largeRadius, yCenter - largeRadius, largeRadius * 2, largeRadius * 2, 0, 180 );
+
+        // Declare and initialize the radii of the small and medium
+        // semicircles and draw them:
+        int smallRadius = height / 4;
+        int mediumRadius = (int)Math.sqrt(smallRadius * largeRadius);
+
+        // Medium Semi Circle
+        g.setColor(Color.GREEN);
+        g.fillArc( xCenter - mediumRadius, yCenter - mediumRadius, mediumRadius * 2, mediumRadius * 2, 0, 180 );
+
+        // Small Semi Circle
+        g.setColor(Color.MAGENTA);
+        g.fillArc( xCenter - smallRadius, yCenter - smallRadius, smallRadius * 2, smallRadius * 2, 0, 180 );
+
+        // Calculate the radius of the innermost (sky-color) semicircle
+        // so that the width of the middle (green) ring is the
+        // arithmetic mean of the widths of the red and magenta rings:
+        int skyRadius = largeRadius - 3 * mediumRadius + 3  * smallRadius;
+        g.setColor(skyColor);
+        g.fillArc( xCenter - skyRadius, yCenter - skyRadius, skyRadius * 2, skyRadius * 2, 0, 180 );
+
+
+    }
 
     public static void main(String[] args)
     {
