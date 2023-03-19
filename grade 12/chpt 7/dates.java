27be584264229cc09d63250e08944a89194a0d48
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-19 15:44:49 -0400

    chapter 7

--- grade 12/chpt 7/dates.java
+++ grade 12/chpt 7/dates.java
@@ -2,36 +2,36 @@ import java.util.Scanner;
 
 public class Dates
 {
-  /**
-   *  returns true if month1/day1/year1 is later than
-   *  month2/day2/year2; otherwise returns false
-   */
-  public static boolean isLater(int month1, int day1, int year1,
+/**
+ *  returns true if month1/day1/year1 is later than
+ *  month2/day2/year2; otherwise returns false
+ */
+public static boolean isLater(int month1, int day1, int year1,
                                 int month2, int day2, int year2)
-  {
+{
     // Check if year1 is later than year2
     if ((year1 > year2))
     {
-      return true;
+    return true;
     }
     // Check if year1 and month1 are later than year2 and month2
     else if ((year1==year2 && month1>month2))
     {
-      return true;
+    return true;
     }
     // Check if year1, month1, and day1 are later than year2, month2, and day2
     else if (year1==year2 && month1==month2 && day1>day2)
     {
-      return true;
+    return true;
     }
     else
     {
-      return false;
+    return false;
     }
-  }
+}
 
-  public static void main(String[] args)
-  {
+public static void main(String[] args)
+{
     Scanner kb = new Scanner(System.in);
 
     // Get the first date from the user
@@ -51,10 +51,10 @@ public class Dates
     // Check which date is later
     String msg = month1 + "/" + day1 + "/" + year1;
     if (isLater(month1, day1, year1, month2, day2, year2))
-      msg += " IS ";
+    msg += " IS ";
     else
-      msg += " is NOT ";
+    msg += " is NOT ";
     msg += "later than " + month2 + "/" + day2 + "/" + year2;
     System.out.println(msg);
-  }
+}
 }
