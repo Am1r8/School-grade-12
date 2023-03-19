97fc889b6b5e053bec0a691639a9cb63752e30ca
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-19 15:41:04 -0400

    chapter 7

--- grade 12/chpt 7/rps.java
+++ grade 12/chpt 7/rps.java
@@ -5,30 +5,39 @@ import javax.swing.*;
 public class Rps extends JFrame
     implements ActionListener
 {
-  private final char moves[] = {'R', 'P', 'S'};
-  private JRadioButton rock, paper, scissors;
-  private JTextField display;
+// Array of moves (rock, paper, scissors)
+private final char moves[] = {'R', 'P', 'S'};
+// Radio buttons for user input
+private JRadioButton rock, paper, scissors;
+// Text field to display game results
+private JTextField display;
 
-  public Rps()
-  {
+// Constructor
+public Rps()
+{
     super("Rock, paper, Scissors");
 
+    // Create radio buttons for each move
     rock = new JRadioButton("   Rock   ", true);
     paper = new JRadioButton("   Paper  ");
     scissors = new JRadioButton(" Scissors ");
 
+    // Group radio buttons so only one can be selected at a time
     ButtonGroup rpsButtons = new ButtonGroup();
     rpsButtons.add(rock);
     rpsButtons.add(paper);
     rpsButtons.add(scissors);
 
+    // Create a "Go" button to start the game
     JButton go = new JButton("         Go         ");
     go.addActionListener(this);
 
+    // Create a text field to display game results
     display = new JTextField(25);
     display.setEditable(false);
     display.setBackground(Color.yellow);
 
+    // Add all components to the content pane
     Container c = getContentPane();
     c.setLayout(new FlowLayout());
     c.add(rock);
@@ -36,94 +45,94 @@ public class Rps extends JFrame
     c.add(scissors);
     c.add(go);
     c.add(display);
-  }
+}
 
-  /**
-   *  returns -1 if the player wins,
-   *  0 if it's a tie, and 1 if the computer wins
-   */
-  private int nextPlay(char computerMove, char playerMove)
-  {
-	  switch(computerMove){
+/**
+ *  returns -1 if the player wins,
+ *  0 if it's a tie, and 1 if the computer wins
+ */
+private int nextPlay(char computerMove, char playerMove)
+{
+    // Determine game outcome based on computer and player moves
+    switch(computerMove){
+    case 'R':
+        switch(playerMove){
+        case 'R':
+            return 0;
+        case 'P':
+            return -1;
+        case 'S':
+            return 1;
+        }
+    case 'P':
+        switch(playerMove){
+        case 'R':
+            return 1;
+        case 'P':
+            return 0;
+        case 'S':
+            return -1;
+        }
+    case 'S':
+        switch(playerMove){
+        case 'R':
+            return -1;
+        case 'P':
+            return 1;
+        case 'S':
+            return 0;
+        }
+    }
+    return 0;
+}
 
-	  case 'R':
-		  switch(playerMove){
-
-		  case 'R':
-			  return 0;
-		  case 'P':
-			  return -1;
-		  case 'S':
-			  return 1;
-		  }
-	  case 'P':
-		  switch(playerMove){
-
-		  case 'R':
-			  return 1;
-		  case 'P':
-			  return 0;
-		  case 'S':
-			  return -1;
-		  }
-	  case 'S':
-		  switch(playerMove){
-
-		  case 'R':
-			  return -1;
-		  case 'P':
-			  return 1;
-		  case 'S':
-			  return 0;
-		  }
-
-	  }
-	  return 0;
-  }
-
-  public void actionPerformed(ActionEvent e)
-  {
+// Handle button clicks
+public void actionPerformed(ActionEvent e)
+{
     char playerMove, computerMove;
     if (rock.isSelected())
-      playerMove = 'R';
+    playerMove = 'R';
     else if (paper.isSelected())
-      playerMove = 'P';
+    playerMove = 'P';
     else // if (scissors.isSelected())
-      playerMove = 'S';
+    playerMove = 'S';
 
+    // Choose a random move for the computer
     int k = (int)(Math.random() * 3);
     computerMove = moves[k];
+    // Determine the game outcome
     int result = nextPlay(computerMove, playerMove);
 
+    // Display the game results in the text field
     String msg = "  You said " + makeWord(playerMove) + ", I said " +
-                 makeWord(computerMove);
+                makeWord(computerMove);
     if (result < 0)
-      msg += " -- you win.";
+    msg += " -- you win.";
     else if (result == 0)
-      msg += " -- tie.";
+    msg += " -- tie.";
     else // if (result > 0)
-      msg += " -- I win.";
+    msg += " -- I win.";
     display.setText(msg);
-  }
+}
 
-  private String makeWord(char move)
-  {
+private String makeWord(char move)
+{
     String word = "";
 
     switch (move)
     {
-      case 'R': word = "ROCK"; break;
-      case 'P': word = "PAPER"; break;
-      case 'S': word = "SCISSORS"; break;
+    case 'R': word = "ROCK"; break;
+    case 'P': word = "PAPER"; break;
+    case 'S': word = "SCISSORS"; break;
     }
     return word;
-  }
+}
 
-  public static void main(String[] args)
-  {
+public static void main(String[] args)
+{
     Rps window = new Rps();
     window.setBounds(300, 300, 300, 140);
     window.setDefaultCloseOperation(EXIT_ON_CLOSE);
     window.setVisible(true);
-  }
+}
 }
